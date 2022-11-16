29e81498694c869c04a3ab5216f875e0713ab04e
danpanorama <62213659+danpanorama@users.noreply.github.com>
2022-11-16 17:22:32 +0200

    some  changes

--- src/components/homecomp/ImageOpen.jsx
+++ src/components/homecomp/ImageOpen.jsx
@@ -20,10 +20,10 @@ import "../../css/App.css";
 
 function ImageOpen(props) {
   return (
-    <div style={{ transform: `translate3d(0, ${ props.offsetY * 0.8 }px,0)`}}  className="openingBg flexcol zindex-1">
+    <div style={{ transform: `translate3d(0, ${ props.offsetY * 0.6   }px,0)`}}  className="openingBg flexcol zindex-1">
 
-      <div style={{ transform: `translate3d(0, -${ props.offsetY * 0.5 }px,0)`}}  className="headers flexcol center " >
-        <h1 className="h1 cw"> Samia & Mali  </h1>
+      <div style={{ transform: `translate3d(0, -${ props.offsetY * 0.6 }px,0)`}}  className="headers flexcol center " >
+        <h1 className="h1 cw"> Samia  <span> &</span> Mali  </h1>
         <h2 className="h2 cw textalinphone">הדבקת ריסים סידור גבות תסרוקות כלה ובניית ציפורניים רחוב יפת 47 יפו</h2>
       </div>
       {/* <div className="endd flexcol center"><div className="kav"></div></div> */}
