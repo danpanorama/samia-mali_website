2c9ebaf842704579a5af4acb38d5fbe070e46a85
danpanorama <62213659+danpanorama@users.noreply.github.com>
2022-11-18 16:19:05 +0200

    set up server

--- src/config/AxiosConfig.jsx
+++ src/config/AxiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3050/',
+  baseURL:process.env.BASE_URL_AWS ||'http://18.182.30.112:3050/',
   // baseURL: "http://localhost:3000/",
 });
 
